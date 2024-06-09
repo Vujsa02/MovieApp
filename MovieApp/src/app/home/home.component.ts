88e8cfd46bc874afe9632cc2549e332f998382a8
milanovicmilos <104598163+milanovicmilos@users.noreply.github.com>
2024-06-09 22:45:03 +0200

    feat: added aws-cdk and created lambda functions for upload and download

--- MovieApp/src/app/home/home.component.ts
+++ MovieApp/src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import {MovieService} from "./movie.service";
 
 @Component({
   selector: 'app-home',
@@ -9,7 +10,9 @@ import { HttpClient } from '@angular/common/http';
 export class HomeComponent {
   selectedFile: File | null = null;  // Variable to store the selected file
 
-  constructor(private http: HttpClient) {}
+  constructor(
+    private service: MovieService  // Inject the service
+  ) {}
 
   // Method to handle file selection
   onFileSelected(event: any) {
@@ -18,24 +21,24 @@ export class HomeComponent {
 
   // Method to download a movie
   downloadMovie() {
-    let file: string = "mudo";
-    return this.http.get("arn:aws:execute-api:eu-central-1:381492024366:65g3v5nudb/*/GET/download/" + file).subscribe(response => {
-      console.log('Download response:', response);
-    });
   }
 
   // Method to upload a movie
   uploadMovie() {
     if (this.selectedFile) {
-      const formData = new FormData();
-      formData.append('file', this.selectedFile, this.selectedFile.name);  // Append the selected file to form data
+      // convert movie to base64
+      const reader = new FileReader();
+      reader.readAsDataURL(this.selectedFile);
+      reader.onload = () => {
+        const fileContent = reader.result;
+        if (fileContent && typeof fileContent === 'string') {
+          this.service.uploadMovie('Movie Title', 'Movie Description', this.selectedFile?.name || 'filmic', fileContent).subscribe(() => {
+            alert('Movie uploaded successfully');
+          });
+        }
 
-      return this.http.post("arn:aws:execute-api:eu-central-1:381492024366:65g3v5nudb/*/POST/upload", formData).subscribe(response => {
-        console.log('Upload response:', response);
-      });
-    } else {
-      console.error('No file selected');
-      return null;
+      };
     }
   }
+
 }
